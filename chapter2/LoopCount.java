2749baa6cc5eabbf8c231a82ebb571089849730a
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-12 00:13:28 +0900

    upload 2-3-2

---
+++ chapter2/LoopCount.java
@@ -0,0 +1,19 @@
+package exe2_3_2;
+
+public class LoopCount {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String loop = "Loop";  //文字列を変数に格納する
+		
+		//反復構造whileで文字列と繰り返し回数を５回表示する
+		int i = 1; 
+		while(i <= 5) {
+			System.out.println(loop + " : " + i + " time(s)");
+			i ++;
+		}
+
+	}
+
+}
