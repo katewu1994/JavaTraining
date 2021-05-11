700496fe81f281051cd5dfe59c3bfb0ec9f5fd1a
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-11 19:29:49 +0900

    upload 1-1-2 Employee

---
+++ chapter1/Employee.java
@@ -0,0 +1,16 @@
+package mod1;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//従業員番号を表示する
+		System.out.println("Employee Code:11011");
+		
+		//名前を出力する
+		System.out.println("Name:Shizuki");
+
+	}
+
+}
