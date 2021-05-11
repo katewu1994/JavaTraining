09a31e7dbfab551a3e63d1864d57f6ceaf743628
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-11 21:28:42 +0900

    upload 2-1-3

---
+++ chapter2/Add.java
@@ -0,0 +1,24 @@
+package exe2_1_3;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//変数の宣言と初期化する
+		int num1 = 100;
+		int num2 = 200;
+		int num3 = 300;
+		
+		//合計値を格納する変数を宣言し、初期化する
+		int sum = 0;
+		
+		//与えられた数値を加算する
+		sum = num1 + num2 +num3;
+		
+		//加算した結果を表示する
+		System.out.print("The sum is " + sum + ".");
+
+	}
+
+}
