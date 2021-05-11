d9c67d8a33bc9b913111b44d1873ef3145539cd2
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-11 21:29:02 +0900

    upload 2-1-4

---
+++ chapter2/Arithmetic.java
@@ -0,0 +1,24 @@
+package exe2_1_4;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//変数の宣言と初期化する
+		int num1 = 15;
+		int num2 = 2;
+		
+		//計算結果を格納する変数を宣言し、初期化する
+		double result = 0;
+		
+		//与えられた数値を除算する
+		result = (double) num1 / num2;
+		
+		//除算した結果を表示する
+		System.out.print("The result is " + result + ".");
+		
+
+	}
+
+}
