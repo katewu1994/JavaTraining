a0e1915d4582ad25adb8745de8c9f0f183fe7342
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-12 00:14:03 +0900

    upload 2-3-3

---
+++ chapter2/Trinangle.java
@@ -0,0 +1,17 @@
+package exe2_3_3;
+
+public class Trinangle {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String star = "";
+		
+		for(int i  = 1; i <= 10; i ++) {
+			star = star + "*";
+			System.out.println(star);
+		}
+
+	}
+
+}
---
+++ chapter2/Trinangle1.java
@@ -0,0 +1,16 @@
+package exe2_3_3;
+
+public class Trinangle1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 0; i < 10; i ++) {
+			for(int j = 0; j <= i; j ++) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+
+	}
+
+}
