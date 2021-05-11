1b16a286686ec8329e136c3711746c9d3a07179b
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-12 00:16:23 +0900

    upload 2-4-3

---
+++ chapter2/Argument.java
@@ -0,0 +1,17 @@
+package exe2_4_3;
+
+public class Argument {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		int sum = a + b;
+		
+		System.out.println(a + " + " + b + " = " + sum);
+
+	}
+
+}
