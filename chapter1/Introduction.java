ab2849412fbd7fd5b844d2c8575e719378fd4d2e
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-11 19:37:21 +0900

    upload 1-1-3 introduction

---
+++ chapter1/Introduction.java
@@ -0,0 +1,18 @@
+package mod1;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//自己紹介の内容を表示する
+		System.out.println("会社名：ラーニングシステム株式会社");
+		System.out.println("氏名　：夏木豊");
+		System.out.println("所属　：事業推進部");
+		System.out.println("内線　：7714-2214");
+		System.out.println("一言　：javaの基礎を完全マスターする！！");
+		
+
+	}
+
+}
