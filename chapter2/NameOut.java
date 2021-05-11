0cd66681ba3d685cdceae05b81db0f544b16b746
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-11 21:28:13 +0900

    upload 2-1-2

---
+++ chapter2/NameOut.java
@@ -0,0 +1,16 @@
+package exe2_1_2;
+
+public class NameOut {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//変数を宣言し、名前を変数に代入する
+		String name = "Shizuki Ichiro";
+		
+		//変数の値を表示する
+		System.out.print(name);
+
+	}
+
+}
