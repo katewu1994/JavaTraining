7e1ede5b301471dc27151872a746886aebf8bf6e
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2021-05-12 00:13:02 +0900

    upload 2-3-1

---
+++ chapter2/LoopOut.java
@@ -0,0 +1,18 @@
+package exe2_3_1;
+
+public class LoopOut {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String loop = "Loop"; //文字列を変数に格納する
+		
+		//反復構造forで文字列を３回表示する
+		for(int i = 1; i <= 3; i ++) {
+			System.out.println(loop);
+		}
+		
+
+	}
+
+}
